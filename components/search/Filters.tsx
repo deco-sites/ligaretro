c9c984e34f16e28d7f78d6168c69f02deb099fa6
Guilherme Baufaker <baufaker@gmail.com>
2024-01-07 20:23:00 -0300

    price filter

--- components/search/Filters.tsx
+++ components/search/Filters.tsx
@@ -7,6 +7,8 @@ import type {
   ProductListingPage,
 } from "apps/commerce/types.ts";
 import { parseRange } from "apps/commerce/utils/filters.ts";
+import { useEffect, useState } from "preact/hooks";
+import MultiRangeSlider from "$store/components/ui/PriceSlider.tsx";
 
 interface Props {
   filters: ProductListingPage["filters"];
@@ -31,6 +33,64 @@ function FilterValues({ key, values }: FilterToggle) {
   const flexDirection = key === "tamanho" || key === "cor"
     ? "flex-row"
     : "flex-col";
+
+  if (key === "price") {
+    //parsing range values for price filter
+    const valuesArray = Object.entries(values);
+
+    const url = valuesArray[0][1].url;
+
+    const urlPrice = url.split("&").slice(0, -1).filter((r) =>
+      r.includes("filter.price")
+    )[0]?.split("=")[1]?.split("%3A");
+    const urlBrowser = url.split("&").slice(0, -1).filter((r) =>
+      !r.includes("filter.price")
+    ).join("&");
+    const rangeArray: number[] = [];
+
+    valuesArray.map((value) => {
+      const aux = value[1].value.split(":");
+      const auxArr = aux.map((r) => parseInt(r));
+      rangeArray.push(...auxArr);
+    });
+    rangeArray.sort((a, b) => a - b);
+    const minRange = rangeArray[0];
+    const maxRange = rangeArray[rangeArray.length - 1];
+
+    const [currentMaxMin, setCurrentMaxMin] = useState({
+      max: urlPrice ? parseInt(urlPrice[1]) : maxRange,
+      min: urlPrice ? parseInt(urlPrice[0]) : minRange,
+    });
+
+    let timeOutId = 0;
+    let firstTime = 0;
+
+    return (
+      <div class={`h-16 mt-4`}>
+        <MultiRangeSlider
+          min={minRange}
+          max={maxRange}
+          currentMin={currentMaxMin.min}
+          currentMax={currentMaxMin.max}
+          onChange={(query: { min: number; max: number }) => {
+            if (
+              currentMaxMin.max != query.max || currentMaxMin.min != query.min
+            ) {
+              if (firstTime > 0) {
+                clearTimeout(timeOutId);
+                timeOutId = setTimeout(() => {
+                  setCurrentMaxMin({ max: query.max, min: query.min });
+                  window.location.href = urlBrowser + "&filter.price=" +
+                    query.min + "%3A" + query.max;
+                }, 500);
+              }
+              firstTime++;
+            }
+          }}
+        />
+      </div>
+    );
+  }
 
   return (
     <ul class={`flex flex-wrap gap-2 ${flexDirection}`}>
