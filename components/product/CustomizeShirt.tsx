6adcb6d37bdd5cdfbf82cc1ff93b2c024d6ee189
Guilherme Baufaker <baufaker@gmail.com>
2024-01-23 12:09:24 -0300

    madee info editable

--- components/product/CustomizeShirt.tsx
+++ components/product/CustomizeShirt.tsx
@@ -2,6 +2,7 @@ import Modal from "$store/components/ui/Modal.tsx";
 import { useUI } from "$store/sdk/useUI.ts";
 import Image from "apps/website/components/Image.tsx";
 import Button from "deco-sites/ligaretro/components/ui/Button.tsx";
+import { useState } from "preact/hooks";
 
 export interface Props {
   productName?: string;
@@ -10,6 +11,43 @@ export interface Props {
 
 function CustomizeModal() {
   const { displayCustomizePopup } = useUI();
+  const [shirtName, setShirtName] = useState("");
+  const [shirtNumber, setShirtNumber] = useState("");
+  const [partialValue, setPartialValue] = useState(0);
+
+  const handleApplyName = () => {
+    let partialCount = partialValue;
+
+    const nameInput = document.getElementById("nameShirt") as HTMLInputElement;
+
+    if (shirtName === "" && nameInput.value !== "") {
+      partialCount += 10;
+    }
+    if (shirtName !== "" && nameInput.value === "") {
+      partialCount -= 10;
+    }
+
+    setShirtName(nameInput.value);
+    setPartialValue(partialCount);
+  };
+
+  const handleApplyNumber = () => {
+    let partialCount = partialValue;
+
+    const numberInput = document.getElementById(
+      "numberShirt",
+    ) as HTMLInputElement;
+
+    if (shirtNumber === "" && numberInput.value !== "") {
+      partialCount += 10;
+    }
+    if (shirtNumber !== "" && numberInput.value === "") {
+      partialCount -= 10;
+    }
+
+    setShirtNumber(numberInput.value);
+    setPartialValue(partialCount);
+  };
 
   return (
     <Modal
@@ -17,8 +55,21 @@ function CustomizeModal() {
       open={displayCustomizePopup.value}
       onClose={() => displayCustomizePopup.value = false}
     >
-      <div class="container flex bg-white p-5 w-fit gap-6 rounded-lg">
-        <div class="p-10 bg-[#e7e7e7] rounded-lg">
+      <div class="absolute container flex bg-white p-5 w-fit gap-6 rounded-lg">
+        <div class="p-10 bg-[#e7e7e7] rounded-lg relative">
+          <span class="absolute left-1/2 transform -translate-x-1/2 bottom-[1%] text-xs w-full text-center">
+            Imagem Ilustrativa - Aplicação será no modelo escolhido
+          </span>
+          {shirtName !== "" && (
+            <span class="absolute left-1/2 transform -translate-x-1/2 uppercase top-[23%] font-bold text-2xl">
+              {shirtName}
+            </span>
+          )}
+          {shirtNumber && (
+            <span class="absolute text-6xl left-1/2 transform -translate-x-1/2 uppercase top-[29%] font-bold text-[120px]">
+              {shirtNumber}
+            </span>
+          )}
           <Image
             src="https://ligaretro.vtexassets.com/assets/vtex.file-manager-graphql/images/96e3da9c-7ed8-4a6f-9f20-bfca9d58579c___fd8ee451f36ce4e437487da7af1dbb68.webp"
             alt="shirt template"
@@ -34,40 +85,47 @@ function CustomizeModal() {
           </div>
           <div class="flex flex-col gap-10">
             <div class="flex flex-col gap-4">
-              <span class="text-xs">Adicione seu nome (R$ 10,00)</span>
+              <span class="text-xs">Adicione um nome (R$ 10,00)</span>
               <div class="join">
                 <input
-                  name="nameShi"
+                  name="nameShirt"
+                  id="nameShirt"
                   class="input focus:border-[#A1A1A1] focus:outline-none  bg-none border border-[#A1A1A1] border-r-0 text-xs w-[230px] join-item rounded-l-lg"
                   type="text"
-                  value={""}
+                  value={shirtName}
                   maxLength={10}
                   placeholder={"Máximo 10 caracteres"}
                 />
                 <Button
                   class="  bg-transparent border border-[#A1A1A1] border-l-0 rounded-r-lg px-2 lg:px-0  w-[120px] h-[25px] rounded-md uppercase text-xs font-bold join-item"
                   type="submit"
-                  htmlFor="nameShi"
+                  htmlFor="nameShirt"
+                  onClick={handleApplyName}
                 >
                   Aplicar
                 </Button>
               </div>
             </div>
             <div class="flex flex-col gap-4">
-              <span class="text-xs">Adicione seu número (R$ 10,00)</span>
+              <span class="text-xs">Adicione um número (R$ 10,00)</span>
               <div class="join">
                 <input
-                  name="nameShi"
+                  name="numberShirt"
+                  id="numberShirt"
                   class="input focus:border-[#A1A1A1] focus:outline-none  bg-none border border-[#A1A1A1] border-r-0 text-xs w-[230px] join-item rounded-l-lg"
-                  type="number"
+                  type="text"
+                  min={0}
+                  max={99}
                   maxLength={2}
-                  value={""}
+                  pattern="[0-99]*"
+                  value={shirtNumber}
                   placeholder={"Máximo 2 caracteres"}
                 />
                 <Button
                   class="  bg-transparent border border-[#A1A1A1] border-l-0 rounded-r-lg px-2 lg:px-0  w-[120px] h-[25px] rounded-md uppercase text-xs font-bold join-item"
                   type="submit"
-                  htmlFor="nameShi"
+                  htmlFor="numberShirt"
+                  onClick={handleApplyNumber}
                 >
                   Aplicar
                 </Button>
@@ -77,7 +135,7 @@ function CustomizeModal() {
           <div class="flex justify-between">
             <div class="flex flex-col font-semibold">
               <span class="text-xs">Valor Parcial</span>
-              <span class="text-xl">R$ 0,00</span>
+              <span class="text-xl">R$ {partialValue.toFixed(2)}</span>
             </div>
             <Button class="bg-black text-white font-normal rounded-lg">
               Finalizar
