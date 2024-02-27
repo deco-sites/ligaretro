1e4ab99950cbaa0e381de746fb37251bafa3d416
Guilherme Baufaker <baufaker@gmail.com>
2024-02-27 16:23:59 -0300

    configured email sending from resale

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -7,14 +7,14 @@ export interface Props {
 
 export const sendEmail = async (
   props: Props,
-  _req: Request
+  _req: Request,
 ): Promise<unknown | null> => {
   const { name, company, email, apiKey } = props;
   try {
-    const response = await fetch(' https://api.sendgrid.com/v3/mail/send', {
-      method: 'POST',
+    const response = await fetch(" https://api.sendgrid.com/v3/mail/send", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
         Authorization: `Bearer ${apiKey}`,
       },
       body: JSON.stringify({
@@ -22,22 +22,23 @@ export const sendEmail = async (
           {
             to: [
               {
-                email: 'faleconosco@ligaretro.com',
+                email: "faleconosco@ligaretro.com",
               },
               {
-                email: 'leonardo@ligaretro.com',
+                email: "leonardo@ligaretro.com",
               },
             ],
           },
         ],
         from: {
-          email: 'faleconosco@ligaretro.com',
+          email: "faleconosco@ligaretro.com",
         },
-        subject: '[Revenda/Franquia] Formulário de contato',
+        subject: "[Revenda/Franquia] Formulário de contato",
         content: [
           {
-            type: 'text/plain',
-            value: `Oi, tenho interesse em conversar sobre as franquias/revendas da Liga Retrô. Meu nome é ${name}, da empresa ${company}, e meu email é ${email} `,
+            type: "text/plain",
+            value:
+              `Oi, tenho interesse em conversar sobre as franquias/revendas da Liga Retrô. Meu nome é ${name}, da empresa ${company}, e meu email é ${email} `,
           },
         ],
       }),
