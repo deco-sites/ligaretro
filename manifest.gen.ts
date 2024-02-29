466afa2fe95abcc6f0884d1f9bacf34466302830
riquezzz <riquezzz@riquezzzs-MacBook-Pro.local>
2024-02-29 14:55:28 -0300

    fix navItems

--- manifest.gen.ts
+++ manifest.gen.ts
@@ -2,9 +2,10 @@
 // This file SHOULD be checked into source version control.
 // This file is automatically updated during development when running `dev.ts`.
 
-import * as $$$0 from "./loaders/Layouts/ProductCard.tsx";
-import * as $$$1 from "./loaders/List/Sections.tsx";
-import * as $$$2 from "./loaders/Reviews/reviewsandratings.ts";
+import * as $$$0 from "./loaders/Collection/listCollectionSlug.ts";
+import * as $$$1 from "./loaders/Layouts/ProductCard.tsx";
+import * as $$$2 from "./loaders/List/Sections.tsx";
+import * as $$$3 from "./loaders/Reviews/reviewsandratings.ts";
 import * as $$$$$$0 from "./sections/Gallery.tsx";
 import * as $$$$$$1 from "./sections/Footer/Footer.tsx";
 import * as $$$$$$2 from "./sections/Category/CategoryBanner.tsx";
@@ -63,9 +64,10 @@ import * as $$$$$$$$$$$1 from "./apps/site.ts";
 
 const manifest = {
   "loaders": {
-    "deco-sites/ligaretro/loaders/Layouts/ProductCard.tsx": $$$0,
-    "deco-sites/ligaretro/loaders/List/Sections.tsx": $$$1,
-    "deco-sites/ligaretro/loaders/Reviews/reviewsandratings.ts": $$$2,
+    "deco-sites/ligaretro/loaders/Collection/listCollectionSlug.ts": $$$0,
+    "deco-sites/ligaretro/loaders/Layouts/ProductCard.tsx": $$$1,
+    "deco-sites/ligaretro/loaders/List/Sections.tsx": $$$2,
+    "deco-sites/ligaretro/loaders/Reviews/reviewsandratings.ts": $$$3,
   },
   "sections": {
     "deco-sites/ligaretro/sections/Animation/Animation.tsx": $$$$$$12,
