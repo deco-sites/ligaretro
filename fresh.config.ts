634790b4c826dcd1447bebdbf5f907343f09e7e2
Diego <diego.o.guimaraes29@gmail.com>
2024-03-08 15:47:45 -0300

    lint

--- fresh.config.ts
+++ fresh.config.ts
@@ -1,16 +1,15 @@
 import { defineConfig } from "$fresh/server.ts";
 import plugins from "https://denopkg.com/deco-sites/std@1.24.2/plugins/mod.ts";
 import partytownPlugin from "partytown/mod.ts";
-import tailwind from "./tailwind.config.ts";
-import manifest from "./manifest.gen.ts";
+import tailwind from "./tailwind.config.ts"
+import decoManifest from "./manifest.gen.ts"
 
 export default defineConfig({
   plugins: [
-    ...plugins({
-      manifest,
-      // deno-lint-ignore no-explicit-any
-      tailwind: tailwind as any,
-    }),
+    ...plugins({ 
+    manifest: decoManifest,
+    // deno-lint-ignore no-explicit-any
+    tailwind: tailwind as any }),
     partytownPlugin(),
   ],
 });
