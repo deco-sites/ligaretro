1c3369a13d69a8d265e1439cd03202d6ce8f7d92
riquezzz <riquezzz@riquezzzs-MacBook-Pro.local>
2024-03-11 15:33:26 -0300

    format

--- components/header/Drawers.tsx
+++ components/header/Drawers.tsx
@@ -30,7 +30,10 @@ const Aside = (
     children: ComponentChildren;
   },
 ) => (
-  <div class="bg-base-100 grid grid-rows-[auto_1fr] min-h-[100vh] h-auto divide-y max-w-[100vw]" style={{ minHeight: "100vh" }}>
+  <div
+    class="bg-base-100 grid grid-rows-[auto_1fr] min-h-[100vh] h-auto divide-y max-w-[100vw]"
+    style={{ minHeight: "100vh" }}
+  >
     <div class="flex justify-between items-center">
       <h1 class="px-4 py-3">
         <span class="font-medium text-2xl">{title}</span>
