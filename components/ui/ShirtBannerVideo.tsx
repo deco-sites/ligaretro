f0c057afefa36915752e79cfdf9f23a2e5871095
Guilherme Baufaker <baufaker@gmail.com>
2024-03-19 19:14:29 -0300

    made videourl optional in shirthistory

--- components/ui/ShirtBannerVideo.tsx
+++ components/ui/ShirtBannerVideo.tsx
@@ -7,7 +7,7 @@ export interface Props {
     image: string;
     alt: string;
   };
-  videoUrl: string;
+  videoUrl?: string;
 }
 
 function ShirtBannerVideo({ banner, videoUrl }: Props) {
@@ -15,19 +15,23 @@ function ShirtBannerVideo({ banner, videoUrl }: Props) {
   const [url, setUrl] = useState(videoUrl);
 
   const handleClick = () => {
-    setHideBanner(true);
-    setUrl(videoUrl + "&autoplay=1");
+    if (videoUrl) {
+      setHideBanner(true);
+      setUrl(videoUrl + "&autoplay=1");
+    }
   };
 
   return (
     <>
       <div
         onClick={handleClick}
-        class={`relative text-white cursor-pointer hover:brightness-110 ${
-          hideBanner && "hidden"
-        }`}
+        class={`relative text-white ${
+          videoUrl && "cursor-pointer"
+        } hover:brightness-110 ${hideBanner && "hidden"}`}
       >
-        <Icon id="Play" size={48} class="absolute right-6 bottom-6" />
+        {videoUrl && (
+          <Icon id="Play" size={48} class="absolute right-6 bottom-6" />
+        )}
         <Image
           width={1360}
           height={678}
@@ -35,21 +39,23 @@ function ShirtBannerVideo({ banner, videoUrl }: Props) {
           alt={banner.alt}
         />
       </div>
-      <div
-        class={`flex justify-center ${
-          !hideBanner && "hidden"
-        } aspect-w-16 aspect-h-8 h-[678px] w-[1360px] max-w-[90vw]`}
-      >
-        <iframe
-          class="w-full h-full"
-          src={url}
-          title="YouTube video player"
-          frameborder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          allowFullScreen
+      {videoUrl && (
+        <div
+          class={`flex justify-center ${
+            !hideBanner && "hidden"
+          } aspect-w-16 aspect-h-8 h-[678px] w-[1360px] max-w-[90vw]`}
         >
-        </iframe>
-      </div>
+          <iframe
+            class="w-full h-full"
+            src={url}
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowFullScreen
+          >
+          </iframe>
+        </div>
+      )}
     </>
   );
 }
